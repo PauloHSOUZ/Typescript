a771f57016dcbcb3d816fb556eb0c7d9e8f6abb6
Paulo Henrique <Henrique@Paulos-MBP.lan>
2019-08-06 23:59:17 -0300

    unums ans tuplas

--- tipos/tipos.ts
+++ tipos/tipos.ts
@@ -24,3 +24,30 @@ console.log(idadeE);
 let possuiHobbiesE: boolean = true
 console.log(possuiHobbiesE);
 
+// Arrays
+// Definindo tipos
+let hobbies: string[] = ["Cozinhar"]
+console.log(hobbies);
+
+// Tuplas
+let endereco: [string,number] = ['Travessa', 99];
+console.log(endereco);
+
+// Unums
+enum Cor {
+    Cinza, // 0
+    Verde = 100, // 100
+    Azul = 10, // 10
+    Laranja, // 11
+    Amarelo, // 12
+    Vermelho = 100
+}
+
+let minhaCor: Cor = Cor.Cinza
+
+console.log(minhaCor);
+console.log(Cor.Azul);
+console.log(Cor.Laranja, Cor.Amarelo);
+console.log(Cor.Verde, Cor.Vermelho);
+
+
