0f64c053800e31e0553d3fd46b0d8018dd071454
Paulo Henrique <Henrique@Paulos-MacBook-Pro.local>
2019-09-18 23:24:57 -0300

    classes

--- classes/classes.js
+++ classes/classes.js
@@ -1,26 +1,26 @@
 "use strict";
-class Data {
-    constructor(dia = 1, mes = 1, ano = 1970) {
-        this.dia = dia,
-            this.mes = mes;
-        this.ano = ano;
-    }
-}
-const aniversario = new Data(3, 11, 1991);
-aniversario.dia = 4;
-console.log(aniversario.dia);
-console.log(aniversario);
-const casamento = new Data;
-casamento.ano = 2017;
-console.log(casamento);
-class DataEsperta {
-    constructor(dia = 1, mes = 1, ano = 1970) {
-        this.dia = dia;
-        this.mes = mes;
-        this.ano = ano;
-    }
-}
-const aniversarioEsperto = new DataEsperta(3, 11, 1991);
+// class Data {
+//     public dia: number;
+//     public mes: number;
+//     public ano: number;
+//     constructor(dia: number = 1, mes: number = 1, ano: number = 1970) {
+//         this.dia = dia,
+//         this.mes = mes;
+//         this.ano = ano;
+//     }
+// }
+// const aniversario = new Data(3, 11, 1991);
+// aniversario.dia = 4;
+// console.log(aniversario.dia);
+// console.log(aniversario);
+// const casamento = new Data
+// casamento.ano = 2017
+// console.log(casamento);
+// class DataEsperta {
+//     constructor(public dia: number = 1, public mes: number = 1, public ano: number = 1970) {
+//     }
+// }
+// const aniversarioEsperto = new DataEsperta(3, 11, 1991);
 class Produto {
     constructor(nome, preco, desconto = 0) {
         this.nome = nome;
@@ -28,10 +28,40 @@ class Produto {
         this.desconto = desconto;
     }
     resumo() {
-        return `${this.nome} custa R$${this.preco} (${this.desconto * 100}% off)`;
+        return `${this.nome} custa R$${this.precoComDesconto()} (${this.desconto * 100}% off)`;
+    }
+    precoComDesconto() {
+        return this.preco - (this.desconto * 100);
     }
 }
 // const carro = new Produto('Carro',37.000);
-const moto = new Produto('Moto', 37.000, 0.1);
-console.log(moto.resumo());
+// const moto = new Produto('Moto',37000,0.5);
+// console.log(moto.precoComDesconto());
+// console.log(moto.resumo());
+class Carro {
+    constructor(marca, modelo, velocidadeMaxima = 200) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.velocidadeMaxima = velocidadeMaxima;
+        this.velocidadeAtual = 0;
+    }
+    alterarVelocidade(delta) {
+        const novaVelocidade = this.velocidadeAtual + delta;
+        const velocidadeValida = novaVelocidade >= 0 && novaVelocidade <= this.velocidadeMaxima;
+        if (velocidadeValida)
+            this.velocidadeAtual = novaVelocidade;
+        else
+            this.velocidadeAtual = delta > 0 ? this.velocidadeMaxima : 0;
+        return this.velocidadeAtual;
+    }
+    acelerar() {
+        return this.alterarVelocidade(5);
+    }
+    frear() {
+        return this.alterarVelocidade(-5);
+    }
+}
+const carro1 = new Carro('Ford', 'Ka', 185);
+Array(50).fill(0).forEach(() => carro1.acelerar());
+console.log(carro1.acelerar());
 //# sourceMappingURL=classes.js.map
--- classes/classes.ts
+++ classes/classes.ts
@@ -1,34 +1,34 @@
-class Data {
+// class Data {
 
-    public dia: number;
-    public mes: number;
-    public ano: number;
+//     public dia: number;
+//     public mes: number;
+//     public ano: number;
 
-    constructor(dia: number = 1, mes: number = 1, ano: number = 1970) {
-        this.dia = dia,
-        this.mes = mes;
-        this.ano = ano;
-    }
-}
+//     constructor(dia: number = 1, mes: number = 1, ano: number = 1970) {
+//         this.dia = dia,
+//         this.mes = mes;
+//         this.ano = ano;
+//     }
+// }
 
-const aniversario = new Data(3, 11, 1991);
+// const aniversario = new Data(3, 11, 1991);
 
-aniversario.dia = 4;
-console.log(aniversario.dia);
-console.log(aniversario);
+// aniversario.dia = 4;
+// console.log(aniversario.dia);
+// console.log(aniversario);
 
-const casamento = new Data
-casamento.ano = 2017
-console.log(casamento);
+// const casamento = new Data
+// casamento.ano = 2017
+// console.log(casamento);
 
 
-class DataEsperta {
-    constructor(public dia: number = 1, public mes: number = 1, public ano: number = 1970) {
+// class DataEsperta {
+//     constructor(public dia: number = 1, public mes: number = 1, public ano: number = 1970) {
 
-    }
-}
+//     }
+// }
 
-const aniversarioEsperto = new DataEsperta(3, 11, 1991);
+// const aniversarioEsperto = new DataEsperta(3, 11, 1991);
 
 
 class Produto {
@@ -36,11 +36,47 @@ class Produto {
     constructor(public nome: string, public preco: number, public desconto: number = 0){}
 
     public resumo(): string {
-        return `${this.nome} custa R$${this.preco} (${this.desconto * 100}% off)`;
+        return `${this.nome} custa R$${this.precoComDesconto()} (${this.desconto * 100}% off)`;
+    }
+
+    public precoComDesconto(): number {
+        return this.preco - (this.desconto * 100);
     }
 }
 
 // const carro = new Produto('Carro',37.000);
-const moto = new Produto('Moto',37.000,0.1);
-console.log(moto.resumo());
+// const moto = new Produto('Moto',37000,0.5);
+// console.log(moto.precoComDesconto());
+// console.log(moto.resumo());
+
+class Carro {
+
+    private velocidadeAtual: number = 0;
+
+    constructor(public marca: string, public modelo: string, private velocidadeMaxima: number = 200){}
+
+    private alterarVelocidade(delta: number): number {
+        const novaVelocidade = this.velocidadeAtual + delta;
+        const velocidadeValida = novaVelocidade >= 0 && novaVelocidade <= this.velocidadeMaxima;
+
+        if (velocidadeValida) this.velocidadeAtual = novaVelocidade;
+        else
+            this.velocidadeAtual = delta > 0 ? this.velocidadeMaxima : 0;
+
+        return this.velocidadeAtual;
+    }
+
+    public acelerar(): number {
+        return this.alterarVelocidade(5);
+    }
+
+    public frear(): number {
+        return this.alterarVelocidade(-5);
+    }
+}
+
+const carro1 = new Carro('Ford', 'Ka', 185);
+
+Array(50).fill(0).forEach(() => carro1.acelerar());
+console.log(carro1.acelerar());
 
